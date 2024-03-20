fb0ebc95743bd2c0d3c5f33f8ebd40e7350d9142
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2024-03-20 08:24:01 -0300

    feat: added and refactored sequelize database tests

--- tests/infrastructure/database/sequelize.test.ts
+++ tests/infrastructure/database/sequelize.test.ts
@@ -1,28 +1,24 @@
 import { ESupportedDatabaseDrivers } from "@/domain/entities/IDatabaseSingletonFactory";
 import { DatabaseSingletonFactory } from "@/infrastructure/database/DatabaseFactory";
-import Sequelize from "@sequelize/core";
+import Sequelize, { sql } from "@sequelize/core";
 
-const connectDatabaseHelper = async (driver: ESupportedDatabaseDrivers) => {
-    const databaseFactory = new DatabaseSingletonFactory();
-    const database = await databaseFactory.createDatabase(driver)
-    return await database.connect()
-}
+const chosenDatabase = ESupportedDatabaseDrivers.SQLITE;
 
-describe("Should correctly connect to the database", () => {
-    describe("Should connect to the sqlite database", () => {
+describe("Should correctly connect to the databases", () => {
+    describe("Should connect to the chosen database: " + chosenDatabase, () => {
         let database: Sequelize;
 
         beforeAll(async () => {
-            database = await connectDatabaseHelper(ESupportedDatabaseDrivers.SQLITE).then((res) => {
+            database = await connectDatabaseHelper(chosenDatabase).then((res) => {
                 if(!(res instanceof Sequelize)) throw new Error('The database is not an instance of Sequelize')
 
                 return res 
             })
         });
 
-        it("Should connect to the sqlite database", async () => {
+        it("Should connect to the chosen database", async () => {
             expect(database).toBeInstanceOf(Sequelize);
-            expect(database.dialect.name).toBe('sqlite');
+            expect(database.dialect.name).toBe(chosenDatabase);
         });
 
         it("Should load the models correctly", async () => {
@@ -31,5 +27,23 @@ describe("Should correctly connect to the database", () => {
             expect(models).toBeDefined();
             expect(models.size).toBeGreaterThan(0);
         });
+
+        it("Should be able to query the database", async () => {
+            const result = await database.query(sql`SELECT 1 + 1 AS result;`);
+
+            expect(result).toBeDefined();
+            expect(result[0][0]).toHaveProperty('result');
+            expect((result[0][0] as any).result).toBe(2);
+        })
+
+        afterAll(async () => {
+            await database.close();
+        });
     });
 });
+
+const connectDatabaseHelper = async (driver: ESupportedDatabaseDrivers) => {
+    const databaseFactory = new DatabaseSingletonFactory();
+    const database = await databaseFactory.createDatabase(driver)
+    return await database.connect()
+}
